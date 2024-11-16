ce5cb14fd2b52ab34247a25037e9b0fee107391e
Sarmesh Kalwar <sarmesh17@gmail.com>
2024-11-16 13:29:23 +0545

    added auth view model

---
+++ app/src/main/java/com/matrix/projectsathi/presentation/viewmodels/AuthViewModel.kt
@@ -0,0 +1,52 @@
+package com.matrix.projectsathi.presentation.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AuthViewModel @Inject constructor(
+    private val auth: FirebaseAuth,
+    private val database: FirebaseDatabase
+):ViewModel() {
+
+
+    private val _signUpState = MutableLiveData<SignUpState>()
+    val signUpState: LiveData<SignUpState> get() = _signUpState
+
+    fun signUp(firstName: String, lastName: String, email: String, password: String) {
+        _signUpState.value = SignUpState.Loading
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val userId = task.result?.user?.uid ?: ""
+                    val user = mapOf(
+                        "firstName" to firstName,
+                        "lastName" to lastName,
+                        "email" to email
+                    )
+
+                    database.reference.child("users").child(userId).setValue(user)
+                        .addOnSuccessListener {
+                            _signUpState.value = SignUpState.Success
+                        }
+                        .addOnFailureListener { exception ->
+                            _signUpState.value = SignUpState.Error(exception.message ?: "Failed to save user data")
+                        }
+                } else {
+                    _signUpState.value = SignUpState.Error(task.exception?.message ?: "Sign-Up Failed")
+                }
+            }
+    }
+}
+
+sealed class SignUpState {
+    object Loading : SignUpState()
+    object Success : SignUpState()
+    data class Error(val message: String) : SignUpState()
+}
