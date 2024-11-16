be5b90efeb50db76664cd84d3af36139d808dcc4
pawon <pawonshrestha11@gmail.com>
2024-11-17 02:24:14 +0545

    added savescreen

---
+++ app/src/main/java/com/matrix/projectsathi/SaveScreen.kt
@@ -0,0 +1,31 @@
+package com.matrix.projectsathi
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showSystemUi = true)
+@Composable
+fun SaveScreen() {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Image(painter = painterResource(id = R.drawable.unavailable), contentDescription = null )
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(text = "Currently not available", fontSize = 22.sp, color = Color.Gray)
+    }
+}
