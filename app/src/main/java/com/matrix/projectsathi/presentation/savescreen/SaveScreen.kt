f6a5b5d5fffd8e90f23b9cd8727d2dda89215af9
pawon <pawonshrestha11@gmail.com>
2024-11-17 03:08:32 +0545

    updating

--- app/src/main/java/com/matrix/projectsathi/presentation/savescreen/SaveScreen.kt
+++ app/src/main/java/com/matrix/projectsathi/presentation/savescreen/SaveScreen.kt
@@ -1,4 +1,4 @@
-package com.matrix.projectsathi
+package com.matrix.projectsathi.presentation.savescreen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -15,6 +15,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.matrix.projectsathi.R
 
 @Preview(showSystemUi = true)
 @Composable
